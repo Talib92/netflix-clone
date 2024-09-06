13373e1af5bb5b8abe8b244f2347da71c7d25fea
Talib Mukhtar <talibmukhtar54@gmail.com>
2024-09-06 11:21:34 +0530

    updated constants folder

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const LOGO = "https://cdn.prod.website-files.com/5ee732bebd9839b494ff27cd/5ee732bebd98393d75ff281d_580b57fcd9996e24bc43c529.png";
+
+export const BG_LOGO = "https://analyticsindiamag.com/wp-content/uploads/2019/05/apps.55787.9007199266246365.687a10a8-4c4a-4a47-8ec5-a95f70d8852d.jpg";
+
+export const API_OPTIONS = 
+    {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGU3OWRmMWFmYWUwZDRiNTRkOGJlZWJhZDc4ODkyMSIsIm5iZiI6MTcyNDkzNzE0NS40MjAzMjgsInN1YiI6IjY2ZDA3MWVjNmFiZTgyZGE4ZmQzMzM0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._nFDfDtAsjEM9Fvx87NnV2hGzfFr9Ns50qlnCAgCfX0'
+        }
+    };
+
+export const IMG_URL = "https://image.tmdb.org/t/p/w500/"
